0d678fbaf6b524f09014475afd3b7e28f3f8ce87
Moritz Hilscher <moritz.hilscher@gmail.com>
2018-11-08 20:07:21 +0100

    Improve variable nodes

--- pyvisual/node/io/value.py
+++ pyvisual/node/io/value.py
@@ -193,14 +193,54 @@ class SetFloatVar(Node):
     def __init__(self):
         super().__init__()
 
+        self.graph = None
+        self.stopped = True
+        # TODO when graph is deserialized that contains duplicate nodes
+        # the order of who is duplicate vs. who is original might change
+        self.duplicate = False
+
+    @property
+    def valid(self):
+        return not self.duplicate and bool(self.name)
+
+    @property
+    def name(self):
+        return self.get("name")
+    @name.setter
+    def name(self, name):
+        self.get_input("name").value = name
+
     def start(self, graph):
+        self.graph = graph
+        self.stopped = False
         SetFloatVar.instances[graph].add(self)
 
     def _show_custom_ui(self):
         imgui.push_item_width(widget.WIDGET_WIDTH)
+        if self.duplicate:
+            imgui.push_style_color(imgui.COLOR_TEXT, 1.0, 0.0, 0.0)
         changed, v = imgui.input_text("", self.get("name"), 255, imgui.INPUT_TEXT_ENTER_RETURNS_TRUE)
+        if self.duplicate:
+            imgui.pop_style_color()
+            if imgui.is_item_hovered():
+                imgui.set_tooltip("Name is a duplicate!")
         if changed:
-            self.get_input("name").value = v
+            self.name = v
+
+    def _evaluate(self):
+        if self.have_inputs_changed("name"):
+            name = self.name
+            for node in SetFloatVar.instances[self.graph]:
+                if node != self and node.valid and node.name == name:
+                    self.duplicate = True
+                    return
+            self.duplicate = False
+
+    def stop(self):
+        assert self.graph is not None
+
+        self.stopped = True
+        SetFloatVar.instances[self.graph].remove(self)
 
 class GetFloatVar(Node):
     class Meta:
@@ -216,10 +256,18 @@ class GetFloatVar(Node):
         }
 
     def __init__(self):
+        # TODO maybe a needs_evaluate method that we can overwrite here?!
         super().__init__(always_evaluate=True)
 
         self.graph = None
         self.connected_node = None
+
+    @property
+    def name(self):
+        return self.get("name")
+    @name.setter
+    def name(self, name):
+        self.get_input("name").value = name
 
     def get_sub_nodes(self, include_self):
         sub_nodes = super().get_sub_nodes(include_self)
@@ -230,15 +278,24 @@ class GetFloatVar(Node):
     def start(self, graph):
         self.graph = graph
 
+    def evaluate(self, *args, **kwargs):
+        if self.connected_node is not None:
+            if self.connected_node.stopped:
+                self.name = ""
+
+        super().evaluate(*args, **kwargs)
+
     def _evaluate(self):
         assert self.graph is not None
 
-        if self.get_input("name").has_changed:
-            name = self.get("name")
+        if self.have_inputs_changed("name"):
+            name = self.name
 
             self.connected_node = None
+            if not name:
+                return
             for node in SetFloatVar.instances[self.graph]:
-                if node.get("name") == name:
+                if node.valid and node.name == name:
                     self.connected_node = node
                     break
 
@@ -246,11 +303,27 @@ class GetFloatVar(Node):
             self.set("output", self.connected_node.get("input"))
 
     def _show_custom_ui(self):
-        imgui.push_item_width(widget.WIDGET_WIDTH)
-        if self.connected_node is None:
-            imgui.push_style_color(imgui.COLOR_TEXT, 1.0, 0.0, 0.0)
-        changed, v = imgui.input_text("", self.get("name"), 255, imgui.INPUT_TEXT_ENTER_RETURNS_TRUE)
-        if self.connected_node is None:
-            imgui.pop_style_color()
-        if changed:
-            self.get_input("name").value = v
+        preview = "<none>"
+        if self.connected_node is not None:
+            preview = self.connected_node.name
+        if imgui.begin_combo("", preview):
+            nodes = SetFloatVar.instances[self.graph]
+
+            is_selected = self.connected_node is None
+            opened, selected = imgui.selectable("<none>", is_selected)
+            if opened:
+                self.name = ""
+            if is_selected:
+                imgui.set_item_default_focus()
+            imgui.separator()
+
+            for node in sorted(nodes, key=lambda node: node.name):
+                if not node.valid:
+                    continue
+                is_selected = node == self.connected_node
+                opened, selected = imgui.selectable(node.name, is_selected)
+                if opened:
+                    self.name = node.name
+                if is_selected:
+                    imgui.set_item_default_focus()
+            imgui.end_combo()
