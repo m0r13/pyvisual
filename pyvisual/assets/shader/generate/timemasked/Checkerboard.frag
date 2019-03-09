8a410671d4f0328e46c1d949a79c30d36a0ca655
m0r13 <moritz.hilscher@gmail.com>
2019-03-09 18:32:29 +0100

    Add smoothness to checkerboard

--- pyvisual/assets/shader/generate/timemasked/Checkerboard.frag
+++ pyvisual/assets/shader/generate/timemasked/Checkerboard.frag
@@ -7,6 +7,14 @@ uniform int uCenter; // {"default" : 1}
 // TODO implement softness!
 uniform float uSoftness0; // {"default" : 0.005}
 uniform float uSoftness1; // {"default" : 0.005}
+uniform float uSoftness2; // {"default" : 0.005}
+uniform float uSoftness3; // {"default" : 0.005}
+
+float dualSmoothstep(float edge0, float edge1, float value, float smoothness0, float smoothness1) {
+    float softValue = 1.0 - smoothstep(edge1-smoothness1*0.5, edge1+smoothness1*0.5, value);
+    softValue *= smoothstep(0.0, smoothness0, value);
+    return softValue;
+}
 
 void generateFrag() {
     if (uCenter > 0) {
@@ -16,11 +24,12 @@ void generateFrag() {
     float modX = 2.0 / (uCountX * 2.0 + 0.0001);
     float modY = 2.0 / (uCountY * 2.0 + 0.0001);
 
-    bool y = mod(pyvisualUV.y, modY) < 0.5 * modY;
-    bool x = mod(pyvisualUV.x, modX) < 0.5 * modX;
+    float x = dualSmoothstep(0.0, 0.5*modX, mod(pyvisualUV.x, modX), uSoftness0, uSoftness1);
+    float y = dualSmoothstep(0.0, 0.5*modY, mod(pyvisualUV.y, modY), uSoftness2, uSoftness3);
 
-    float value = (x && !y) || (!x && y) ? 1.0 : 0.0;
+    // non-fuzzy equivalent to next line: (x && !y) || (!x && y)
+    float v = abs(x - y);
 
-    pyvisualOutColor = vec4(vec3(value), 1.0);
+    pyvisualOutColor = vec4(vec3(v), 1.0);
 }
 
--- pyvisual/node/op/gpu/custom_meta.py
+++ pyvisual/node/op/gpu/custom_meta.py
@@ -1,6 +1,52 @@
 import random
 
-def values_with_softness(base_values):
+def checkerboard_values_with_softness(base_values):
+    def _generate(node):
+        values = dict(base_values)
+
+        choice = random.choice(["none", "none", "none", "soft01", "soft23"])
+
+        def one_of_two():
+            return random.choice([[0.2, 0.005], [0.005, 0.2]])
+
+        s0, s1, s2, s3 = {
+            "none" : lambda: [0.005, 0.005, 0.005, 0.005],
+            "soft01" : lambda: one_of_two() + [0.005, 0.005],
+            "soft23" : lambda: [0.005, 0.005] + one_of_two(),
+        }[choice]()
+
+        values["uSoftness0"] = s0
+        values["uSoftness1"] = s1
+        values["uSoftness2"] = s2
+        values["uSoftness3"] = s3
+        return values
+    return _generate
+
+class CheckerboardMeta:
+    presets = [
+        ("vertical stripes", checkerboard_values_with_softness({
+            "uCountX" : 3.5,
+            "uCountY" : 0.0,
+            "uCenter" : 0
+        })),
+        ("horizontal stripes", checkerboard_values_with_softness({
+            "uCountX" : 0.0,
+            "uCountY" : 2.5,
+            "uCenter" : 0
+        })),
+        ("16:9 almost square", checkerboard_values_with_softness({
+            "uCountX" : 2.5,
+            "uCountY" : 1.5,
+            "uCenter" : 0
+        })),
+        ("4 centered rects + a bit", checkerboard_values_with_softness({
+            "uCountX" : 1.8,
+            "uCountY" : 1.43,
+            "uCenter" : 1
+        }))
+    ]
+
+def frame_values_with_softness(base_values):
     def _generate(node):
         values = dict(base_values)
 
@@ -17,47 +63,23 @@ def values_with_softness(base_values):
         return values
     return _generate
 
-class CheckerboardMeta:
-    presets = [
-        ("vertical stripes", values_with_softness({
-            "uCountX" : 3.5,
-            "uCountY" : 0.0,
-            "uCenter" : 0
-        })),
-        ("horizontal stripes", values_with_softness({
-            "uCountX" : 0.0,
-            "uCountY" : 2.5,
-            "uCenter" : 0
-        })),
-        ("16:9 almost square", values_with_softness({
-            "uCountX" : 2.5,
-            "uCountY" : 1.5,
-            "uCenter" : 0
-        })),
-        ("4 centered rects + a bit", values_with_softness({
-            "uCountX" : 1.8,
-            "uCountY" : 1.43,
-            "uCenter" : 1
-        }))
-    ]
-
 class FramesMeta:
     presets = [
-        ("square frame blah", values_with_softness({
+        ("square frame blah", frame_values_with_softness({
             "uCount" : 1.97,
             "uFrameSize" : 1.0,
             "uAspectCorrection" : 0.0,
             "uMode" : 0,
             "uInvert" : lambda node: random.randint(0, 1)
         })),
-        ("square frame variations", values_with_softness({
+        ("square frame variations", frame_values_with_softness({
             "uCount" : 1.95,
             "uFrameSize" : 1.11,
             "uAspectCorrection" : 0.5,
             "uMode" : lambda node: random.randint(0, 1),
             "uInvert" : lambda node: random.randint(0, 1)
         })),
-        ("center circle + ring", values_with_softness({
+        ("center circle + ring", frame_values_with_softness({
             "uCount" : 0.91,
             "uFrameSize" : 1.46,
             "uAspectCorrection" : 1.0,
