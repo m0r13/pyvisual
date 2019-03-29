9bafd7117eacb731f6ff54c5387bb44e0790c68c
Moritz Hilscher <moritz.hilscher@gmail.com>
2019-03-29 10:20:06 +0100

    Improve filters to start at current value when it is created (or cutoff is changed!)

--- pyvisual/audio/util.py
+++ pyvisual/audio/util.py
@@ -10,9 +10,13 @@ class Filter:
     def __init__(self, filter_type, order, freq, sample_freq, filter_kwargs):
         norm_freq = freq / (sample_freq * 0.5)
         self._b, self._a = filter_type(order, norm_freq, **filter_kwargs)
-        self._zf = signal.lfiltic(self._b, self._a, np.array([0.0]))
+        self._zf = signal.lfilter_zi(self._b, self._a)
+        self._first_block = True
 
     def process(self, block):
+        if self._first_block:
+            self._first_block = False
+            self._zf = self._zf * block[0]
         out, self._zf = signal.lfilter(self._b, self._a, block, zi=self._zf)
         return out
 
